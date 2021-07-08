de920086e83309a4c56c15a3b59b716e0b2bf70e
philsMINT <philipp.scharpf@uni-konstanz.de>
2021-07-08 14:47:55 +0200

    Change latex2sympy module

--- evaluation/module3_identifier_value_generation/unit_test_module3.py
+++ evaluation/module3_identifier_value_generation/unit_test_module3.py
@@ -1,9 +1,13 @@
-import latex2sympy
+#import latex2sympy
+#from process_latex import process_sympy
+from latex2sympy2 import latex2sympy
 
 defining_formula = input('Enter defining formula: ')
 
 try:
-    formula_sympy = latex2sympy.strToSympy(defining_formula)
+    formula_sympy = latex2sympy(defining_formula)
+    #formula_sympy = latex2sympy.strToSympy(defining_formula)
+    #formula_sympy = process_sympy(defining_formula)
     print('\nSympy format is')
     print(str(formula_sympy))
 except:
